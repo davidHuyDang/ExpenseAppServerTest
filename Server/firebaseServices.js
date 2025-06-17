00a8a56e936cc114e8deb9900730f216947257cf
davidHuyDang <davidhuydang02@gmail.com>
2025-06-17 18:41:28 -0400

    Deploy

--- Server/firebaseServices.js
+++ Server/firebaseServices.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+//serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 require("dotenv").config();
 
